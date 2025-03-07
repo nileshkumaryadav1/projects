d1b2430c088b162c796429d25baff2d1197553ec
Nilesh Kumar <kumarnileshnayan@gmail.com>
2025-03-08 00:46:22 +0530

    added responsiveness

--- app/page.js
+++ app/page.js
@@ -15,10 +15,10 @@ export default function Page() {
   const [activePage, setActivePage] = useState('academic');
 
   return (
-    <div className="flex flex-row items-start p-5 bg-gray-100 min-h-screen">
+    <div className="md:flex md:flex-row items-start md:p-5 bg-gray-100 md:min-h-screen">
       {/* Left Sidebar Navigation */}
-      <div className="flex flex-col w-1/4 p-4 mr-5 bg-white rounded-lg shadow-md overflow-hidden">
-        <h2 className="text-lg font-semibold text-gray-700 mb-2">Navigation</h2>
+      <div className="flex flex-col md:w-1/4 p-4 md:mr-5 mb-2 bg-white rounded-lg shadow-md overflow-hidden">
+        <h2 className="text-lg font-semibold text-gray-700 mb-2 text-center">Navigation</h2>
         {pages.map((page) => (
           <button
             key={page.id}
@@ -34,7 +34,7 @@ export default function Page() {
       </div>
 
       {/* Content Display Area */}
-      <div className="p-3 w-3/4 max-w-5xl rounded-lg shadow-xl bg-white flex flex-col items-center" style={{ marginLeft: '-4px' }}>
+      <div className="p-3 md:w-3/4 md:max-w-5xl rounded-lg shadow-xl bg-white flex flex-col items-center" style={{ marginLeft: '-4px' }}>
         {/* <h4 className="text-xl font-semibold text-gray-800 mb-4">{pages.find((page) => page.id === activePage)?.name}</h4> */}
         <a href={pages.find((page) => page.id === activePage)?.content} target='_blank' >Visit full Website➡️</a>
         <iframe src={pages.find((page) => page.id === activePage)?.content} width="100%" height="600px" style={{ border: 'none', borderRadius: '10px' }} />
